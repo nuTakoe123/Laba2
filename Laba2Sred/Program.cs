f269bd27ed03a4fb09684b7a843c939f0d61d202
nu_takoe123 <v.bzychkin@yandex.ru>
2026-04-20 22:27:09 +0300

    Задание 1

---
+++ Laba2Sred/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задание 1
+
+using System; // Где-то читал, что уже не надо прописовать базовые юзинги
+
+Console.Write("Вводи температуру: ");
+string tempa = Console.ReadLine() ?? "";
+
+if (int.Parse(tempa) < 0)
+{
+    Console.WriteLine("Холодно, надень куртку.");
+}
+else
+{
+    Console.WriteLine("Тепло, можно гулять.");
+}
+// Ответ на котрольный вопрос: Будет ошибка. Можно использовать try/TryParse чтоб отловить ее
+
