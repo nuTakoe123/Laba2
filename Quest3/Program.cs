bbc22861a21a2a571376dc442b3e2e5da70b8862
nu_takoe123 <v.bzychkin@yandex.ru>
2026-04-20 22:54:30 +0300

    задание 3

---
+++ Quest3/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace  Quest3;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine());
+        
+        string result = (number % 2 == 0) ? "четное" : "нечетное";
+        Console.WriteLine($"Число {number} — {result}.");
+
+    }
+}
